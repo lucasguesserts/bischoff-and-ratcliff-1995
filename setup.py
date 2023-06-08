b924f7aee89a3f0d54f71f98257857c7fb981a83
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2023-06-08 16:09:05 -0300

    build: add pyproject.toml and publish at PyPI

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     # Metadata
     name='bischoff_and_ratcliff_1995',
     version='0.0.0',
+    description='Instance generator, as described in the paper of Bischoff and Ratcliff (1995)',
     url='https://github.com/lucasguesserts/bischoff-and-ratcliff-1995',
     author='Lucas Guesser',
     author_email='lucasguesserts@gmail.com',
