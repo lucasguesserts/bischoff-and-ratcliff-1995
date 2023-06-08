34e4b0704717edc9acff0fd4ab6a2e62f3cfd819
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2023-06-08 16:10:20 -0300

    build: add binary file to package

--- setup.py
+++ setup.py
@@ -2,26 +2,25 @@ from setuptools import setup
 
 setup(
     # Metadata
-    name='bischoff_and_ratcliff_1995',
-    version='0.0.0',
-    description='Instance generator, as described in the paper of Bischoff and Ratcliff (1995)',
-    url='https://github.com/lucasguesserts/bischoff-and-ratcliff-1995',
-    author='Lucas Guesser',
-    author_email='lucasguesserts@gmail.com',
+    name="bischoff_and_ratcliff_1995",
+    version="0.0.0",
+    description="Instance generator, as described in the paper of Bischoff and Ratcliff (1995)",
+    url="https://github.com/lucasguesserts/bischoff-and-ratcliff-1995",
+    author="Lucas Guesser",
+    author_email="lucasguesserts@gmail.com",
     classifiers=[
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Topic :: Scientific/Engineering',
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering",
     ],
-	keywords=[
-        'container loading problem',
-        'knapsack problem',
-        'single large object placement problem (slopp)',
-        'packing problem'
+    keywords=[
+        "container loading problem",
+        "knapsack problem",
+        "single large object placement problem (slopp)",
+        "packing problem",
     ],
     # Options
-    install_requires=[
-        'pytest'
-    ],
-    python_requires='>=3.0',
+    install_requires=["pytest"],
+    python_requires=">=3.0",
+    package_data={"pkg_name": ["bin/generate_all_instances"]},
 )
